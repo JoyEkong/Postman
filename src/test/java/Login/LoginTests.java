490b6294b96950b61c6fe04ac6284bcb7d74fec5
JoyEkong <joyekong123@gmail.com>
2020-03-27 00:47:54 +0200

    push first commit

---
+++ src/test/java/Login/LoginTests.java
@@ -0,0 +1,35 @@
+package Login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginTests {
+
+    private WebDriver driver;
+
+    public void setUp() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://identity.getpostman.com/login?addaccount=1");
+        Thread.sleep(5000);
+
+        //1. maximize the window
+        driver.manage().window().maximize();
+
+        System.out.println(driver.getTitle());
+        driver.findElement(By.id("email")).sendKeys("joyekong123@gmail.com");
+        driver.findElement(By.id("pass")).sendKeys("081355145");
+        driver.findElement(By.id("loginbutton")).click();
+
+    }
+       public static void main(String args[]) throws InterruptedException {
+            LoginTests tests = new LoginTests();
+            tests.setUp();
+    }
+
+
+
+}
